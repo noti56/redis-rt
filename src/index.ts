3d020533634438379c46ef71e18d390c0fe077d8
Netanel Halevi <netanel.h@zemingo.com>
2024-10-21 11:28:56 +0300

    lib config

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
-import { z } from "zod";
-import { Model } from "./Model";
-import { RedisClient } from "./RedisClient";
-RedisClient.getInstance({ url: "redis://localhost:6379" });
+// import { z } from "zod";
+// import { Model } from "./Model";
+// import { RedisClient } from "./RedisClient";
+// RedisClient.getInstance({ url: "redis://localhost:6379" });
 
-const movement = new Model("movement", z.object({ x: z.number(), y: z.number() }));
-(async () => {
-  const subscription = await movement.listenTo("1");
-  subscription.subscribe((x) => console.log(x));
+// const movement = new Model("movement", z.object({ x: z.number(), y: z.number() }));
+// (async () => {
+//   const subscription = await movement.listenTo("1");
+//   subscription.subscribe((x) => console.log(x));
 
-  setTimeout(() => {
-    // Correct way to update the model
-    movement.update("1", { y: 10, x: 52 });
-  }, 3000);
-})();
+//   setTimeout(() => {
+//     movement.update("1", { y: 10, x: 52 });
+//   }, 3000);
+// })();
+
+export * from "./types";
+export * from "./BaseModel";
+export * from "./Model";
+export * from "./RedisClient";
